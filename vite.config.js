a64cd8c1dc3da5b7043e15b8bffa7dc1988635ae
Pranay <pranay.work@example.com>
2026-04-15 18:14:14 +0530

    fix: add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Solar_radiation-sensor_data_cleaning_tool/',
   plugins: [react()],
 })
